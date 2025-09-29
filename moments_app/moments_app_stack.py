989bcd835afdbf96d96e8069e5cfbd9cabad3900
iamjustinthecloud <iamjustinthecloud@gmail.com>
2025-09-29 12:23:42 +0200

    feat: create S3 bucket resource

--- moments_app/moments_app_stack.py
+++ moments_app/moments_app_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     Stack,
     aws_ssm as ssm,
+    aws_s3 as s3, RemovalPolicy,
 )
 from constructs import Construct
 
@@ -11,6 +12,10 @@ class MomentsAppStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         self.create_stack_name_ssm_parameter()
+        self.create_s3_bucket()
+
+
+    # Resource creation
 
     def create_stack_name_ssm_parameter(self) -> ssm.StringParameter:
         # --- Persist stack name in SSM ---
@@ -20,4 +25,12 @@ class MomentsAppStack(Stack):
             description="The name of the Moments App Stack",
             parameter_name="MomentsAppStackName",
             string_value=self.stack_name,
+        )
+
+    def create_s3_bucket(self):
+        return s3.Bucket(
+            self,
+            "MomentsBucket",
+            removal_policy=RemovalPolicy.DESTROY,
+            bucket_name="moments-app-bucket",
         )
