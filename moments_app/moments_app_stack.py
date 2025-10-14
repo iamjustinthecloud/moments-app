fe36cf05e04c642f7c0f430ea5a3fd1f4e3ad5de
iamjustinthecloud <iamjustinthecloud@gmail.com>
2025-10-14 10:09:07 +0200

    fix: set removal policy on cdk managed log group

--- moments_app/moments_app_stack.py
+++ moments_app/moments_app_stack.py
@@ -1,17 +1,16 @@
 from aws_cdk import (
     Stack,
-    aws_ssm as ssm,
     aws_s3 as s3,
     RemovalPolicy,
     Duration,
     aws_sqs as sqs,
-    aws_events as events,
-    aws_events_targets as targets,
     aws_lambda as lambda_,
     aws_logs as logs,
     aws_lambda_event_sources as lambda_event_sources,
 )
 from constructs import Construct
+
+GMAIL_INGESTOR = "GmailIngestor"
 
 
 class MomentsAppStack(Stack):
@@ -19,93 +18,77 @@ class MomentsAppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        self.create_stack_name_ssm_parameter()
-        self.create_s3_bucket()
+        self.create_s3_static_site_bucket()
         # --- Queues ---
-        dlq = self.create_hello_dlq()
-        hello_queue: sqs.IQueue = self.create_hello_queue(dlq)  # type: ignore
-
-        # --- Add SQS target to the rule ---
-        self.create_hello_rule().add_target(targets.SqsQueue(hello_queue))  # type: ignore
+        gmail_ingestor_dlq = self.create_gmail_ingestor_dlq()
+        gmail_ingestor_queue = self.create_gmail_ingestor_queue(gmail_ingestor_dlq)  # type: ignore
 
         # --- Create Lambda subscribed to SQS ---
-        self.create_scheduled_hello_lambda(hello_queue=hello_queue)  # type: ignore
+        self.create_gmail_ingestor_lambda(gmail_ingestor_queue=gmail_ingestor_queue)  # type: ignore
 
     # Resource creation
 
-    def create_stack_name_ssm_parameter(self) -> ssm.StringParameter:
-        """Persist stack name in SSM"""
-        return ssm.StringParameter(
-            self,
-            "MomentsAppStackName",
-            description="The name of the Moments App Stack",
-            parameter_name="MomentsAppStackName",
-            string_value=self.stack_name,
-        )
-
-    def create_s3_bucket(self):
-        """Create the S3 Bucket."""
+    def create_s3_static_site_bucket(self):
+        """Create the S3 Bucket with web assets."""
         return s3.Bucket(
             self,
-            "MomentsBucket",
+            "MomentsStaticSiteBucket",
             removal_policy=RemovalPolicy.DESTROY,
-            bucket_name="moments-app-bucket",
+            bucket_name="moments-static-site-bucket",
         )
 
-    def create_hello_queue(self, dlq: sqs.IQueue) -> sqs.Queue:
+    def create_gmail_ingestor_queue(self, gmail_ingestor_dlq: sqs.IQueue) -> sqs.Queue:
         """Create the main queue with a DLQ."""
         return sqs.Queue(
             self,
-            "HelloQueue",
-            queue_name="HelloQueue",
+            f"{GMAIL_INGESTOR}Queue",
+            queue_name=f"{GMAIL_INGESTOR}Queue",
             dead_letter_queue=sqs.DeadLetterQueue(
                 max_receive_count=3,
-                queue=dlq,
+                queue=gmail_ingestor_dlq,
             ),
         )
 
-    def create_hello_dlq(self) -> sqs.Queue:
+    def create_gmail_ingestor_dlq(self) -> sqs.Queue:
         """Create the dead-letter queue."""
         return sqs.Queue(
             self,
-            "HelloDLQ",
-            queue_name="HelloDLQ",
+            f"{GMAIL_INGESTOR}DLQ",
+            queue_name=f"{GMAIL_INGESTOR}DLQ",
             retention_period=Duration.days(14),
         )
 
-    def create_hello_rule(self):
-        """Create the CloudWatch Event Rule"""
-        return events.Rule(
+    def create_gmail_ingestor_lambda(
+        self,
+        gmail_ingestor_queue: sqs.IQueue,
+    ) -> lambda_.Function:
+
+        log_group = logs.LogGroup(
             self,
-            "HelloLambdaRule",
-            rule_name="HelloLambdaRule",
-            schedule=events.Schedule.cron(minute="*"),
+            f"{GMAIL_INGESTOR}LogGroup",
+            log_group_name=f"/aws/lambda/{GMAIL_INGESTOR}Lambda",
+            retention=logs.RetentionDays.ONE_YEAR,
+            removal_policy=RemovalPolicy.DESTROY,
         )
 
-    def create_scheduled_hello_lambda(
-        self,
-        hello_queue: sqs.IQueue,
-    ) -> lambda_.Function:
-        """Create a Lambda function triggered by SQS."""
-        hello_lambda_fn = lambda_.Function(
+        gmail_ingestor_lambda_fn = lambda_.Function(
             self,
-            "HelloLambda",
+            f"{GMAIL_INGESTOR}Lambda",
             runtime=lambda_.Runtime.PYTHON_3_12,  # type: ignore
-            handler="hello.handler",
+            handler="gmail_ingestor.handler",
             code=lambda_.Code.from_asset("lambdas"),
             timeout=Duration.seconds(10),
             memory_size=128,
             environment={"LOG_LEVEL": "INFO"},
-            log_retention=logs.RetentionDays.ONE_WEEK,
-            function_name="HelloLambda",
+            function_name=f"{GMAIL_INGESTOR}Lambda",
         )
-
-        # Ensure the managed log group is deleted with the stack.
-        hello_lambda_fn.log_group.apply_removal_policy(RemovalPolicy.DESTROY)
-
-        # Attach SQS event source
-        hello_lambda_fn.add_event_source(
-            lambda_event_sources.SqsEventSource(hello_queue)
+        gmail_ingestor_lambda_fn.node.add_dependency(log_group)
+        gmail_ingestor_lambda_fn.add_event_source(
+            lambda_event_sources.SqsEventSource(
+                gmail_ingestor_queue,
+                batch_size=10,
+                max_batching_window=Duration.seconds(30),
+                report_batch_item_failures=True,
+            )
         )
-
-        return hello_lambda_fn
+        return gmail_ingestor_lambda_fn
